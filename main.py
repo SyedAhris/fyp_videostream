cb5ae3e23a344d8bf956d748e157026e83c9c172
syedma <ahrissyed@gmail.com>
2023-05-07 21:17:12 +0500

    changed code for all videos in videos folder

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 
 from starlette.responses import StreamingResponse
@@ -13,9 +15,9 @@ app = FastAPI()
 data = Data()
 
 
-def read_file():
+def read_file(video_dir):
     while True:
-        cap = cv2.VideoCapture('./Videos/test_video_short.mp4')
+        cap = cv2.VideoCapture(video_dir)
         while cap.isOpened():
             # Capture frame-by-frame
             ret, frame = cap.read()
@@ -30,7 +32,10 @@ def read_file():
                 # convert JPEG-encoded frame to bytes
                 bytes_frame = encoded_frame.tobytes()
 
-                data.data['01'] = bytes_frame
+                #split video_dir to get stream_id
+                stream_id = video_dir.split('/')[-1].split('.')[0]
+                # print(stream_id)
+                data.data[stream_id] = bytes_frame
                 # Press Q on keyboard to  exit
                 # if cv2.waitKey(25) & 0xFF == ord('q'):
                 #     break
@@ -42,13 +47,15 @@ def read_file():
         # When everything done, release the video capture object
         cap.release()
 
+for videos in os.listdir('./Videos'):
+    video_dir = os.path.join('./Videos', videos)
+    t = Thread(target=read_file, args=(video_dir, ))
+    t.start()
 
-t = Thread(target=read_file, args=())
-t.start()
 
-def send_frame():
+def send_frame(stream_id: str):
     while True:
-        frame_bytes = data.data['01']
+        frame_bytes = data.data[stream_id]
         # print(self.stream_frames[stream_name])
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
@@ -64,7 +71,7 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-@app.get('/01')
-async def stream_video():
-    return StreamingResponse(send_frame(),
+@app.get('/{stream_id}')
+async def stream_video(stream_id: str):
+    return StreamingResponse(send_frame(stream_id),
                              media_type="multipart/x-mixed-replace; boundary=frame")
